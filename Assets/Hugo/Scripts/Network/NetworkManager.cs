21bdeef7e98aa1de4784e0dcdf23ef181b4f58f1
Kamil Misiowiec <mkakus@gmail.com>
2016-08-06 12:06:46 +0200

    airconsole plugin

---
+++ Assets/Hugo/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using NDream.AirConsole;
+using System;
+
+public class NetworkManager : MonoBehaviour
+{
+    void Start()
+    {
+        AirConsole.instance.onConnect += OnPlayerConnect;
+        AirConsole.instance.onDisconnect += OnPlayerDisconnect;
+    }
+
+    private void OnPlayerConnect(int device_id)
+    {
+        Debug.Log("Player connected id: " + device_id);
+    }
+
+    private void OnPlayerDisconnect(int device_id)
+    {
+        Debug.Log("Player disconnected id: " + device_id);
+    }
+}
