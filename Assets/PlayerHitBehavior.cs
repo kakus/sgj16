1c3ced22df6fdf708f8416c10da8c3fe23c2f9f6
Karol Furmann <karol.furmann@gmail.com>
2016-08-06 18:38:01 +0200

    Obstacles, bonuses, physics fixes

---
+++ Assets/PlayerHitBehavior.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+public class PlayerHitBehavior : MonoBehaviour
+{
+
+    public bool hit = false;
+    public float upwardForce = 10;
+    public float forwardForce = 10;
+    public GameObject scoreLabel;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!hit)
+        {
+            GameObject otherParent = null;
+            Vector3 randUp = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+            Vector3 randForward = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+
+            randUp *= 0.1f;
+            randForward *= 0.1f;
+            if (other.tag == "Obstacle")
+            {
+                other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                //other.gameObject.GetComponent<OutOfBounds>().enabled = false;
+
+                other.gameObject.GetComponent<Rigidbody>().AddForce(((Camera.main.transform.forward + randForward) * forwardForce + (Vector3.up + randUp) * upwardForce), ForceMode.Impulse);
+                other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+                other.gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-300, 300), Random.Range(-300, 300), Random.Range(-300, 300)), ForceMode.Impulse);
+            }
+            if (other.tag == "Medal")
+            {
+                //Destroy(other.gameObject);
+                // Main.createdMedals.Remove(other.gameObject);
+                // other.gameObject.transform.position = Camera.main.ScreenToWorldPoint(scoreLabel.transform.position);
+               
+                //other.gameObject.transform.position = Camera.main.transform.position+ new Vector3(-2,0.6f,1.4f);
+                other.gameObject.GetComponent<OutOfBounds>().enabled = false;
+                other.gameObject.GetComponent<TweenPos>().targetv3 = Camera.main.transform.position + new Vector3(-2, 2.6f, 1.4f);
+                other.gameObject.GetComponent<TweenPos>().enabled = true;
+                other.gameObject.transform.parent = Camera.main.transform;
+
+                //GameObject scoreLabel = GameObject.FindGameObjectWithTag("ScoreLabel");
+                //scoreLabel.GetComponent<Text>().text = "MEDALE: 5";
+
+                //Destroy(other.gameObject);
+                //Main.createdObstacles.Remove(other.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/TweenPos.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TweenPos : MonoBehaviour {
+
+    public Vector3 targetv3;
+    public GameObject scoreLabel;
+	// Use this for initialization
+	void Start () {
+       
+	    scoreLabel = GameObject.FindGameObjectWithTag("ScoreLabel");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, targetv3, .05f);
+        if(((targetv3 - transform.position).magnitude < 0.1)||(transform.localPosition.z<0))
+        {
+           
+            //if (gameObject.tag=="Medal")
+            {
+                Main.score += 1;
+                scoreLabel.GetComponent<Text>().text = "MEDALE: " + Main.score.ToString();
+
+                Main.createdMedals.Remove(gameObject);
+                Destroy(gameObject);
+            }
+        }
+	}
+}
