805fb7e83a4868c6acc056264346b33c30b24349
Arangear <koadrer@gmail.com>
2016-08-07 01:17:45 +0200

    Audio Now

--- Assets/Hugo/Scripts/DeathByCollision.cs
+++ Assets/Hugo/Scripts/DeathByCollision.cs
@@ -4,12 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class DeathByCollision : MonoBehaviour {
 
+    public AudioSource youfuckedup;
     public float ToWait = 3f;
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if (/*col.gameObject.name == "Bullet(Clone)" /*|| */col.gameObject.name == "Character")
         {
+            youfuckedup.Play();
             Destroy(col.gameObject);
             Destroy(gameObject);
             restartCurrentScene();
